ec47adf2edba8277cd8f54cac2236ecdd76bc70a
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2026-01-28 19:27:23 -0300

    feat(api): expose NexusGraph and MarketAgentState in public API

--- src/nexus_equitygraph/__init__.py
+++ src/nexus_equitygraph/__init__.py
@@ -1,2 +1,6 @@
+"""Nexus EquityGraph Public API."""
 
+from nexus_equitygraph.domain.state import MarketAgentState
+from nexus_equitygraph.graph_runner import NexusGraph
 
+__all__ = ["NexusGraph", "MarketAgentState"]
